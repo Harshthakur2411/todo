6b5f34b3b4a344b080009c8cce73d5c84bfac961
Harshitthakur2411 <rjharshthakur1103@gmail.com>
2025-05-21 14:44:14 +0530

    change the port number

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 7000;
 app.listen(port,() =>{
-   console.log("Server is running 3000");
+   console.log("Server is running 7000");
 });
